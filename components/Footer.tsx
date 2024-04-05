3ca0eea02bf5b1bac2671a853949b1fac9d72cd3
Roy Wenang R <119590801+roywenangr@users.noreply.github.com>
2024-04-06 04:27:28 +0900

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,19 +10,19 @@ export const Footer = () => {
     >
       <ul className="flex text-center ">
         <li className="mr-2.5 lg:mr-10 cursor-pointer">
-          <Link href="https://github.com/philippetedajo">
+          <Link href="https://github.com/roywenangr">
             <a target="_blank" rel="noreferrer noopener" aria-label="github">
               <RiGithubFill size={20} className="hover:text-third" />
             </a>
           </Link>
         </li>
-        <li className="mr-2.5 lg:mr-10 cursor-pointer">
+{/*         <li className="mr-2.5 lg:mr-10 cursor-pointer">
           <Link href="https://www.linkedin.com/">
             <a target="_blank" rel="noreferrer noopener" aria-label="linkedin">
               <RiLinkedinFill size={20} className="hover:text-third" />
             </a>
           </Link>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
